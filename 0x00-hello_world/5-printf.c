b7c88fe98d29dc48f41ab14b5a667401016c8bce
oramah <orahmassah@gmail.com>
2023-03-18 23:25:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,8 @@
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
+
